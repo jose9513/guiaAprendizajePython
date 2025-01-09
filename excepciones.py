6414ef6599d9ebd3038db5f9abe160a2996c5dd0
josexo <jose1820mendoza@gmail.com>
2025-01-09 13:14:32 -0500

    ampliando los ejercicios de manejo de execpciones

--- excepciones.py
+++ excepciones.py
@@ -29,7 +29,7 @@ finally:
 except ValueError as e:
     print(f"Error: {e}")"""
     
-while True:
+"""while True:
     try:
         numero = int(input("ingrese un numero entero: "))
         if numero % 2 != 0:
@@ -38,4 +38,38 @@ while True:
             print(f"el numero que ingreso es: {numero}")
             break
     except ValueError as e:
-        print(e)
+        print(e)"""
+        
+def sumar():
+    """manejo de exvepciones simple con ValueError"""
+    try:
+        numero1 = int(input("ingrese un numero: "))
+        numero2 = int(input("ahora ingrese otro numero: "))
+        suma = numero1 + numero2
+        print(suma)
+    except ValueError:
+        print("porfavor ingrese solo numeros")
+        
+def raiz():
+    """manejo de excepciones para la raiz de un numero que se pide al usuario"""
+    try:
+        numero = int(input("ingrese un numero para hallar la raiz cuadrada: "))
+        raiz = numero ** 0.5
+        if numero < 0:
+            raise ValueError("el numero no puede ser negativo")
+        elif numero % 2 != 0:
+            raise ValueError("el numero no puede ser impar")
+        else:
+            print(raiz)
+    except ValueError:
+        print("ingrese solo numeros por favor")
+        
+"""lista = [10, 20, 30, 40, 50]
+
+try:
+    indice = int(input("ingrese el indice dentro de la lista: "))
+    print(f"el elemento en el indice {indice} es {lista[indice]}")
+except ValueError:
+    print("por favor solo ingrese solo numero por favor")
+except IndexError:
+    print("el indice ingresado no existe dentro de la lista")"""
