7866c811a09373a6dad735c699c8a4f68b41e717
jose <jose1820mendoza@gmail.com>
2025-02-06 16:13:08 -0500

    actualizacion: actualice el archivo 'diccionarios.py' con el contenido de 'creacion de diccionarios'

---
+++ diccionarios.py
@@ -0,0 +1,30 @@
+"""
+  ------------------------
+//CREACION DE DICCIONARIOS//
+  ------------------------
+"""
+
+"""
+#los diccionarios son una opcion para manejar datos que se dividen en pares 'key:value' separados por comas
+mi_diccionario = {
+    "nombre" : "Jose",
+    "edad" : 23,
+    "nacionalidad" : "Peruano"
+}
+print(mi_diccionario)
+"""
+
+"""
+#otra forma de crear diccionarios es con 'dict' y hay diferenes formas de usar dict
+mi_diccionario = dict([
+    ("nombre", "Jose"),
+    ("edad", 23),
+    ("nacionalidad", "Peruano")
+])
+print(mi_diccionario)
+
+mi_otro_diccionario = dict(nombre = "Jose",
+                           edad = 23,
+                           nacionalidad = "Peruano")
+print(mi_otro_diccionario)
+"""
