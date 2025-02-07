e0713973362ec635074dab944e59aadc136cddc5
jose <jose1820mendoza@gmail.com>
2025-02-07 16:07:52 -0500

    actualizacion: actualice el archivo diccionarios.py y agregue los temas de 'acceder y modificar elementos de un diccionario' y 'iteracion en un diccionario'

--- diccionarios.py
+++ diccionarios.py
@@ -28,3 +28,75 @@ mi_otro_diccionario = dict(nombre = "Jose",
                            nacionalidad = "Peruano")
 print(mi_otro_diccionario)
 """
+
+"""
+  -----------------------------------------------
+//ACCEDER Y MODIFICAR ELEMENTOS EN UN DICCIONARIO//
+  -----------------------------------------------
+"""
+
+"""
+#se puede acceder a un elemento con '[]' o con el componente 'get'
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+print(mi_diccionario["nombre"])
+print(mi_diccionario.get("edad"))
+"""
+
+"""
+#puedes agregar elementos poniendo la 'key' entre '[]' e igualandolo con el valor que desees
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+mi_diccionario["calle"] = "calle123"
+print(mi_diccionario.get("calle"))
+"""
+
+"""
+  ---------------------------
+//ITERACION EN UN DICCIONARIO//
+  ---------------------------
+"""
+
+"""
+#usamos el bucle for para iterar entre las diferentes 'keys' del diccionario
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+for x in mi_diccionario:
+  print(x)
+"""
+
+"""
+#usamos el bucle for para iterar entre los diferentes 'values' del diccionario
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+for x in mi_diccionario:
+  print(mi_diccionario[x])
+"""
+
+"""
+#tambien se pueden iterar entre las 'keys' y los 'value' a la misma vez
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+for x, y in mi_diccionario.items():
+  print(x, y)
+"""
+
+"""
+#tambien se pueden crear diferentes formas de imprimir las 'keys' junto con los 'values' del diccionario
+mi_diccionario = {
+  "nombre" : "jose",
+  "edad" : 23
+}
+for x, y in mi_diccionario.items():
+  print(f"{x} : {y}")
+"""
