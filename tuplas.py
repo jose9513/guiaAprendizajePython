95319939465e340f0108f349e34fe73ba96b258c
jose <jose1820mendoza@gmail.com>
2025-02-08 10:58:54 -0500

    nuevo: cree un nuevo archivo llamado 'tuplas.py' y añadi el contenido de 'creacion de tuplas'

---
+++ tuplas.py
@@ -0,0 +1,23 @@
+"""
+  ------------------
+//CREACION DE TUPLAS//
+  ------------------
+"""
+
+"""
+#las tuplas se crean entre parentesis
+mi_tupla = (1, 2, 3, 4, 5)
+print(mi_tupla)
+"""
+
+"""
+#las tuplas tambien se pueden declarar sin parentesis
+mi_tupla = 1, 2, 3, 4, 5
+print(type(mi_tupla))
+print(mi_tupla)
+"""
+
+
+
+mi_tupla = (1, 2, 3, 4, 5)
+mi_tupla[0] = 5
